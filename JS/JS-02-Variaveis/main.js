f5e5fc7012f8efc08122e1ca2654c0eaae29749e
lucasPereira07 <lucascd895@gmail.com>
2024-05-08 21:44:26 -0300

    Aula 02

---
+++ JS/JS-02-Variaveis/main.js
@@ -0,0 +1,44 @@
+//objeto console
+console.log("Olá mundo");
+
+
+//variaveis
+//quero criar um, espaço na memória do computador chamado _felippe e dizer que esse espaço na memória irá receber o valor 8
+var _felippe = 8;
+_felippe = "Neymar"; //redeclarar uma variavel
+console.log(_felippe);
+
+let letsGo = 4; //let não posso redeclarar
+letsGo = 3;
+console.log(letsGo);
+
+console.clear()
+
+//const
+const PI = 3.1415;
+console.log(PI)
+
+let NAN = 0 / 0;
+console.log(NAN);
+
+let corinthians = "Melhor time, Palmeiras sem mundial";
+console.log(corinthians);
+
+var numero1 = 2; 
+var texto = "Eu tenho ";
+var texto2 = " Reais";
+console.log(texto + numero1 + texto2);
+//concatenar = é possivel concatenar strings ou concatenar variaveis
+
+//valores booleanos
+
+var num1 = 1;
+var num2 = 5;
+// num é igual 1 a num2
+// 1 é igual a 1 = true
+
+// console.log( num1 == num2);
+
+var teste;
+
+
