1788b39f94be5dc939dd8b8c0125c700a206c909
lucasPereira07 <lucascd895@gmail.com>
2024-05-09 21:42:33 -0300

    Aula 02 continuação

--- JS/JS-02-Variaveis/main.js
+++ JS/JS-02-Variaveis/main.js
@@ -1,44 +1,82 @@
-//objeto console
-console.log("Olá mundo");
+// //objeto console
+// console.log("Olá mundo");
 
 
-//variaveis
-//quero criar um, espaço na memória do computador chamado _felippe e dizer que esse espaço na memória irá receber o valor 8
-var _felippe = 8;
-_felippe = "Neymar"; //redeclarar uma variavel
-console.log(_felippe);
+// //variaveis
+// //quero criar um, espaço na memória do computador chamado _felippe e dizer que esse espaço na memória irá receber o valor 8
+// var _felippe = 8;
+// _felippe = "Neymar"; //redeclarar uma variavel
+// console.log(_felippe);
 
-let letsGo = 4; //let não posso redeclarar
-letsGo = 3;
-console.log(letsGo);
+// let letsGo = 4; //let não posso redeclarar
+// letsGo = 3;
+// console.log(letsGo);
 
+// console.clear()
+
+// //const
+// const PI = 3.1415;
+// console.log(PI)
+
+// let NAN = 0 / 0;
+// console.log(NAN);
+
+// let corinthians = "Melhor time, Palmeiras sem mundial";
+// console.log(corinthians);
+
+// var numero1 = 2; 
+// var texto = "Eu tenho ";
+// var texto2 = " Reais";
+// console.log(texto + numero1 + texto2);
+// //concatenar = é possivel concatenar strings ou concatenar variaveis
+
+// //valores booleanos
+
+// var num1 = 1;
+// var num2 = 5;
+// // num é igual 1 a num2
+// // 1 é igual a 1 = true
+
+// // console.log( num1 == num2);
+
+// var teste;
+
+//operadores
+var operador1 = 8;
+var operador2 = null;
+console.log(operador1 * operador2);
+
+var operador3 = "5";
+var operador4 = 1;
+console.log(operador3 - operador4);
+
+var operador5 = "5";
+var operador6 = 1;
+console.log(operador5 + operador6);
+
+var operador7 = "five";
+var operador8 = 2;
+console.log(operador7 * operador8);
 console.clear()
 
-//const
-const PI = 3.1415;
-console.log(PI)
+var valor1 = 2;
+var valor2 = 2;
+var resultado = valor1 + valor2;
+console.log(resultado);
 
-let NAN = 0 / 0;
-console.log(NAN);
+var compara = "2" == 2; //só compara o valor
+console.log(compara);
 
-let corinthians = "Melhor time, Palmeiras sem mundial";
-console.log(corinthians);
-
-var numero1 = 2; 
-var texto = "Eu tenho ";
-var texto2 = " Reais";
-console.log(texto + numero1 + texto2);
-//concatenar = é possivel concatenar strings ou concatenar variaveis
-
-//valores booleanos
-
-var num1 = 1;
-var num2 = 5;
-// num é igual 1 a num2
-// 1 é igual a 1 = true
-
-// console.log( num1 == num2);
-
-var teste;
+var compara = "2" === 2; //compara o valor e o tipo
+console.log(compara);
 
 
+let valor3 = 4;
+let valor4 = 6;
+let resultado1 = valor3 > valor4 && valor3 == valor4;
+
+console.log(resultado1);
+
+var valor5 = "5";
+
+console.log(typeof valor5);
